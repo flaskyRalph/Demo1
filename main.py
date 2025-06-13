096d4f01a7c9bed526141778226886266a9fa54f
flaskyRalph <ralph.monteadora21@gmail.com>
2025-06-13 14:43:09 +0800

    add profile feature

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
  
